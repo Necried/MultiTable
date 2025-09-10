e4bba8c8f056ad4f821d94099a30d9d238632fc8
Necried <ldutton888@gmail.com>
2025-09-10 17:38:40 -0400

    SP Rsqrt update

---
+++ src/Single/RSqrt/float_utils.c
@@ -0,0 +1,252 @@
+#include "float_utils.h"
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+
+double float_internal_to_double(float_internal fix) {
+  double x;
+  if (fix.exp >= -1022) {                          // normalized
+    x = fix.significand * pow(2.0, -1 * FRACBITS); // fraction
+    x += 1;                                        // assumed 1
+    x *= pow(2.0, fix.exp);                        // exponent
+  } else {                                         // denormalized
+    printf("float_internal_to_double:  denormalized x not implemented\n");
+    assert(0);
+  }
+  return x;
+}
+
+// Because the range is in [1,2), when mantissa is 0, assume the floating point
+// double is 0;
+double mantissa_to_double(unsigned __int128 mantissa) {
+
+  // printf("mantissa %016llx %016llx \n",(unsigned long long)mantissa,(unsigned
+  // long long)condition);
+  double result;
+  result = ((double)mantissa) / pow(2.0, fracBits);
+  return result;
+  /*
+  if (mantissa < condition){
+
+    mantissa -= (1ull << (fracBits)); // take away the imply one
+    return float_internal_to_double ((float_internal){0,-1, (unsigned long long)
+  mantissa}); // FIX }else{
+
+    mantissa -= (1ull << (fracBits+1));
+    // printf("case2 mantissa_to_double %016llx \n",(unsigned long
+  long)mantissa); return float_internal_to_double ((float_internal){0,0,
+  (unsigned long long) mantissa}); // FIX
+  }
+  */
+}
+
+// mantissa with imply bit
+//  double_to_mantissa works in a interval of [0.5,2.0)
+unsigned __int128 double_to_mantissa(double x) {
+  double_union_t ux;
+  unsigned __int128 mantissa = 0;
+  ux.d = x;
+  // printf("exp %d %016llX ", fix.exp, fix.exp);
+  int exp;
+  exp = (ux.ull & 0x7FF0000000000000ull) >> 52;
+  exp -= 1023; // subtract bias
+  mantissa += (1ull << (fracBits + exp)) /*implybit*/;
+  // int shift = x < 1.0 ?  FRACBITS - 1 : FRACBITS;
+  mantissa += (fracBits + exp) > 52
+                  ? (ux.ull & 0x000FFFFFFFFFFFFFull) << ((fracBits + exp) - 52)
+                  : (ux.ull & 0x000FFFFFFFFFFFFFull) >>
+                        (52 - (fracBits + exp)); // without assumed 1
+
+  // printf("x %10.10f u %016llx exp %d %016x \n",x ,ux.ll,exp,exp);
+  // fix = exp < 0 ? fix + (1ull << (FRACBITS-1)) : fix + (1ull << (FRACBITS));
+  // // add imply 1  printf("fix %016llX \n",fix);
+  return mantissa;
+}
+
+unsigned __int128 double_to_mantissa_withBit(double x, long long bits) {
+  double_union_t ux;
+  unsigned __int128 mantissa = 0;
+  ux.d = x;
+  // printf("exp %d %016llX ", fix.exp, fix.exp);
+  int exp;
+  exp = (ux.ull & 0x7FF0000000000000ull) >> 52;
+  exp -= 1023; // subtract bias
+  mantissa += (1ull << (bits + exp)) /*implybit*/;
+  // int shift = x < 1.0 ?  FRACBITS - 1 : FRACBITS;
+  mantissa += (bits + exp) > 52
+                  ? (ux.ull & 0x000FFFFFFFFFFFFFull) << ((bits + exp) - 52)
+                  : (ux.ull & 0x000FFFFFFFFFFFFFull) >>
+                        (52 - (bits + exp)); // without assumed 1
+
+  // printf("mantissa %016llx x %10.10f u %016llx exp %d %016x \n",mantissa,x
+  // ,ux.ull,exp,exp);  fix = exp < 0 ? fix + (1ull << (FRACBITS-1)) : fix +
+  // (1ull
+  // << (FRACBITS)); // add imply 1  printf("fix %016llX \n",fix);
+  return mantissa;
+}
+
+unsigned long long float_to_fixed23(float x) {
+  float_union_t ux;
+  ux.f = x;
+  int exp = (ux.i & 0x7F800000) >> 23;
+  exp -= 127;
+  unsigned long long significand = (1ull << (23 + exp)) /*implybit*/;
+  significand +=
+      exp > 0 ? (ux.i & 0x007FFFFF) << exp : (ux.i & 0x007FFFFF) >> (-exp);
+  assert(!(exp < 0) || significand < (1ull << 23));
+  return significand;
+}
+
+// Convert a float to fixed point precision with precision of 23 bits
+unsigned long long float_to_fixed23_given_exp(float x, int unbiased_exp) {
+  float_union_t ux;
+  ux.f = x;
+  int exp = (ux.i & 0x7F800000) >> 23;
+  exp -= 127;
+  assert(exp == unbiased_exp);
+  unsigned long long significand = (1ull << 23) /*implybit*/;
+  significand += ux.i & 0x007FFFFF;
+  return significand;
+}
+
+// Convert a float to fixed point precision with specified precision bits
+unsigned long long float_to_fixed23_withprec(float x, unsigned int prec) {
+  float_union_t ux;
+  unsigned long long significand;
+  ux.f = x;
+  int exp = (ux.i & 0x7F800000) >> 23;
+  exp -= 127; // subtract bias
+  unsigned long long implied_one =
+      exp < 0 ? (1ull << 23) >> (-exp) : (1ull << 23) << exp;
+  unsigned long long fraction = exp < 0
+                                    ? (ux.i & 0x007FFFFF) >> (prec - 23 - exp)
+                                    : (ux.i & 0x007FFFFF) << (prec - 23 + exp);
+  // printf("implied_one: %016llx\n", implied_one);
+  significand = fraction | implied_one;
+  return significand;
+}
+
+int count_leading_zeros(unsigned long long x) {
+  int n = 0;
+  while (x < (1ull << 63)) {
+    n++;
+    x <<= 1;
+  }
+  return n;
+}
+
+float fixed23_to_float_given_exp(unsigned long long x, int unbiased_exp) {
+  float result;
+  result = ((float)x) / pow(2.0, 23 - unbiased_exp);
+  return result;
+}
+
+// Convert internal float struct representation to IEEE float.
+float float_internal_to_float(float_internal fix) {
+  float x;
+  if (fix.exp >= -126) {                 // normalized
+    x = fix.significand * pow(2.0, -23); // fraction
+    x += 1;                              // assumed 1
+    x *= pow(2.0, fix.exp);              // exponent
+  } else {                               // denormalized
+    printf("float_internal_to_float:  denormalized x not implemented\n");
+    assert(0);
+  }
+  return x;
+}
+
+double float_internal_to_double_mast(float_internal fix, int masBits) {
+  double x;
+  if (fix.exp >= -1022) {                         // normalized
+    x = fix.significand * pow(2.0, -1 * masBits); // fraction
+    x += 1;                                       // assumed 1
+    x *= pow(2.0, fix.exp);                       // exponent
+  } else {                                        // denormalized
+    printf("float_internal_to_double:  denormalized x not implemented\n");
+    assert(0);
+  }
+  return x;
+}
+
+// round to zero
+float_internal double_to_float_internal(double x) {
+  float_internal fix;
+  double_union_t ux;
+  ux.d = x;
+  fix.sign = (x >= 0 ? 0 : 1);
+  fix.exp = ((ux.ull & 0x7FF0000000000000ull) >> (64 - 12)) - 1023;
+  // printf("exp %d %016llX ", fix.exp, fix.exp);
+  fix.significand =
+      (ux.ull & 0x000FFFFFFFFFFFFFull) >> (52 - FRACBITS); // without assumed 1
+  // printf("ull  %016llX  signif %016llX \n",ux.ll , fix.significand);
+  return fix;
+}
+
+// Convert IEEE float to internal float struct representation.
+float_internal float_to_float_internal(float x) {
+  float_internal fix;
+  float_union_t ux;
+  ux.f = x;
+  fix.sign = (x >= 0 ? 0 : 1);
+  fix.exp = ((ux.i & 0x7F800000) >> 23) - 127; // subtract IEEE bias
+  fix.significand = ux.i & 0x007FFFFF;         // without assumed 1
+  return fix;
+}
+
+double mypow2(int expo) {
+  double res;
+  // Compute 2**expo being careful about pow() problems with denorms
+  if (expo >= -1022) {
+    res = pow(2.0, expo);
+  } else {
+    res = pow(2.0, expo + 53) * pow(2.0, -53);
+  }
+  return res;
+}
+
+double ulp_d(double y) {
+  long long expobits;
+  int expo;
+  union {
+    double d;
+    unsigned long long ull;
+  } u;
+  u.d = y;
+  // printf("u.ull    =%08llX\n", u.ull);
+
+  expobits = u.ull & 0x7FF0000000000000ull;
+  // printf("expobits=%08X\n", expobits);
+  expo = expobits >> (64 - 12);
+  // printf("expo    =%08X=%d\n", expo, expo);
+  expo -= 1023;
+  // printf("expo    =%08X=%d\n", expo, expo);
+  if (expo >= -1022) {
+    return mypow2(expo - FRACBITS);
+  } else {
+    printf("ulp_d denormalized\n");
+    assert(0);
+    return mypow2(-149);
+  }
+}
+
+float ulp_s(float y) {
+  unsigned int expobits;
+  int expo;
+  union {
+    float f;
+    int i;
+  } u;
+  u.f = y;
+  // printf("u.i    =%08X\n", u.i);
+  expobits = u.i & 0x7F800000;
+  // printf("expobits=%08X\n", expobits);
+  expo = expobits >> (32 - 9);
+  // printf("expo    =%08X=%d\n", expo, expo);
+  expo -= 127;
+  // printf("expo    =%08X=%d\n", expo, expo);
+  if (expo >= -126) {
+    return mypow2(expo - 23);
+  } else {
+    return mypow2(-149);
+  }
+}
---
+++ src/Single/RSqrt/rsqrt.c
@@ -0,0 +1,217 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <assert.h>
+
+#include "float_defs.h"
+#include "float_utils.h"
+#include "rsqrt.h"
+
+void makeTTable(__uint64_t* table) {
+  int i;
+  for (i=0;i<RSQRT_TABLE_SIZE/2;i++) {
+    double num =  1 + oneOverSize * i;
+
+    double rsqrtx = 1/sqrt(num);
+    if (i==0) {
+      table[i] =  (1ull<<(fracBitsTable+1)) << (fracBits-(fracBitsTable+1));
+      table[256] = (__int64_t)(floor(1/sqrt(2) * (1ull<<fracBitsTable) * 2)) << (fracBits-(fracBitsTable+1));
+    } else {
+      table[i] = (__int64_t)(floor(rsqrtx * (1ull<<fracBitsTable) * 2)) << (fracBits-(fracBitsTable+1));
+      num =  2 + oneOverSize * 2 * i;
+      rsqrtx = 1/sqrt(num);
+      table[i+256] = (__int64_t)(floor(rsqrtx * (1ull<<fracBitsTable) * 2)) << (fracBits-(fracBitsTable+1));
+    }
+  }
+}
+
+void makeSTable(__uint64_t* table, __uint64_t* T_Table) {
+
+  table[0] = ((T_Table[0])
+    - (T_Table[1]));
+  for (int i=1;i<RSQRT_TABLE_SIZE-1;i++){
+    table[i] = (T_Table[i]-T_Table[i+1]);
+  }
+  table[RSQRT_TABLE_SIZE-1] =
+     ((T_Table[(RSQRT_TABLE_SIZE-1)])
+      - ((1ull<<(fracBits-1))));
+}
+
+
+unsigned __int128 approxRSqrtLinear(unsigned __int128 x, __uint64_t* T_Table, __uint64_t* S_Table) {
+    __uint64_t leadingBit = ((1ull << (fracBits+1)) & x) == 0 ? 0 : 1;
+    __uint64_t newLowu = leadingBit == 0 ? lowu : lowu + 1;
+    __uint64_t idx = ((1ull<<highf)-1) & (((__uint64_t) x) >> newLowu);
+    idx += leadingBit << highf;
+
+    signed __int128 lowBits = ((1ull<<newLowu)-1) & (x);
+
+    // the first exponent in the table is 0 (for value 1.0) and -1 for the rest (1/2 < value < 1)
+    bool idx_isOne = idx == 0 ;
+    __uint64_t tableSignificand = (T_Table[idx]);
+
+    signed __int128 S_Tableslope = (S_Table[idx]); //less than 20 bits
+
+    S_Tableslope *= lowBits;
+
+    signed __int128 l ;
+    l = tableSignificand;// 50
+    l <<= newLowu;
+
+
+    l -= S_Tableslope;
+    l >>= newLowu;
+
+    return l;
+}
+
+void makeQTable (__uint64_t* table, __uint64_t* T_Table, __uint64_t* S_Table){
+  // input x
+  // oneOverSize = 1 / 256
+  int count = 0;
+  // first half range [1,2)
+  for(int id = 0 ; id < RSQRT_TABLE_SIZE/2 ; id ++) {
+    for (int alpha = 0 ; alpha < pow(2,qTableBit); alpha += 1){
+      double x1 = 1
+	+ ((id * pow(2,qTableBit)) + alpha)
+	* (oneOverSize * (1/pow(2,qTableBit))); //1.000244140625
+      double x2 = 1
+	+ ((id * pow(2,qTableBit)) + (alpha + 1))
+	* (oneOverSize * (1/pow(2,qTableBit))); //1.00048828125
+      double step = pow (2, -23);
+      __int64_t result = 0;
+
+      for(double x = x1 ; x <= x2 ; x += step){
+
+	double rsqrtx = 1/sqrt(x);
+	unsigned __int128 tmp = double_to_mantissa (x);
+	unsigned __int128 l_estimate = approxRSqrtLinear(tmp, T_Table, S_Table);
+	double l = mantissa_to_double(l_estimate);
+
+	__int64_t sub = (__int64_t)(floor((l - rsqrtx) * pow(2,23)));
+	__int64_t tmpR = result;
+	result = x == x1?/*init result*/ sub : (result > sub ? sub : result);
+	if (result < 0) result = 0;
+
+	if(tmpR != result){
+	  //printf("x %16.16f l %16.16f rsqrtx %16.16f before floor %10.10f sub %lld result %lld %016llx\n",x,l,rsqrtx,(l - rsqrtx) * pow(2,23),sub,result,result);
+	   }
+	}// end of x
+
+      table [count] = result;
+      count ++;
+    }// end of alpha
+  } // end of id
+  // second half range [2,4)
+  for(int id = 0 ; id < RSQRT_TABLE_SIZE/2 ; id ++) {
+    for (int alpha = 0 ; alpha < pow(2,qTableBit); alpha += 1){
+      double x1 = 2
+	+ ((id * pow(2,qTableBit)) + alpha)
+	* (oneOverSize * 2 * (1/pow(2,qTableBit))); //1.000244140625
+      double x2 = 2
+	+ ((id * pow(2,qTableBit)) + (alpha + 1))
+	* (oneOverSize * 2 * (1/pow(2,qTableBit))); //1.00048828125
+      double step = pow (2, -23); //check
+      __int64_t result = 0;
+
+      for(double x = x1 ; x <= x2 ; x += step){
+
+	double rsqrtx = 1/sqrt(x);
+	unsigned __int128 tmp = double_to_mantissa (x);
+	unsigned __int128 l_estimate = approxRSqrtLinear (tmp, T_Table, S_Table);
+	double l = mantissa_to_double(l_estimate);
+
+	__int64_t sub = (__int64_t)fabs(floor (((l - rsqrtx) * pow(2,23))));
+	__int64_t tmpR = result;
+	result = x == x1?/*init result*/ sub : (result > sub ? sub : result);
+	if (result < 0) result = 0;
+
+	if(tmpR != result){
+	  //printf("x %16.16f l %16.16f rsqrtx %16.16f before floor %10.10f sub %lld result %lld %016llx\n",x,l,rsqrtx,(l - rsqrtx) * pow(2,23),sub,result,result);
+	   }
+	}// end of x
+
+      table [count] = result;
+      count ++;
+    }// end of alpha
+  } // end of id
+  /*
+  count = 0;
+  printf("make q table \n");
+  for(int id = 0 ; id < SQRT_TABLE_SIZE ; id ++) {
+    for (int alpha = 0 ; alpha < pow(2,qTableBit); alpha += 1){
+      printf ("(%i , %016llx)\n",alpha,table[count]);
+
+      count ++;
+    }
+    printf("\n");
+  }
+  exit (0);
+  */
+}
+
+void makeATable(__uint8_t *table, __uint64_t *qTable) {
+  for (int i = 0; i < RSQRT_TABLE_SIZE; i++) {
+    int qTableIdx = ((1ull << qTableBit) / 2) + (i * (1ull << qTableBit));
+    table[i] = qTable[qTableIdx];
+  }
+}
+
+unsigned __int128 three_table_procedure(unsigned __int128 x,
+                                        __uint64_t *T_Table,
+                                        __uint64_t *S_Table,
+                                        __uint8_t *A_Table) {
+  // Extract indices of x
+  __uint32_t leadingBit = ((1ull << (fracBits + 1)) & x) == 0 ? 0 : 1;
+  __uint32_t newLowu = leadingBit == 0 ? lowu : lowu + 1;
+  __uint32_t x0__8 = (leadingBit << highf) + (((1ull << highf) - 1) &
+                     (((__uint64_t)x) >> newLowu));
+  assert(0 <= x0__8 && x0__8 < 512);
+
+  __uint32_t x9__18 =
+      ((1ull << qTableBit) - 1) & (((__uint64_t)x) >> (newLowu - qTableBit));
+  __uint32_t x9__23 = ((1ull << newLowu) - 1) & (x);
+
+
+  // Perform table lookups
+  __uint32_t t = T_Table[x0__8];
+  __uint32_t s = S_Table[x0__8];
+  __uint32_t a = A_Table[x0__8];
+  
+  // Calculate z
+  // Here, we first compute the complement of x[9..18] separately
+  // The complement operator in C produces extraneous leading bits, so we need
+  // to mask.
+  __uint32_t x9__18_comp = ((1 << qTableBit) - 1) & (~x9__18);
+  __uint32_t z = x9__18 * x9__18_comp;
+  // Ensures that the result of the multiplication is at most 22 bits wide
+  // assert(z < (1 << 22));
+
+  // The base shift amount of Q is 18, but we take into account extra precision
+  // introduced by fracBits. We subtract by 23 (the single precision
+  // significand)
+  __uint32_t Q = (z * a) >> (18 - (fracBits - 23));
+  __uint32_t L = ((__uint64_t)s * x9__23) >> newLowu;
+  // The difference from recip is we add the correction instead of subtract.
+  __uint32_t y = t - L - Q;
+
+  return y;
+}
+
+double calculate_ytrue(unsigned __int128 x){
+  return 1/sqrt(mantissa_to_double(x));
+}
+
+double calculate_ytest(unsigned __int128 x,__uint64_t* T_Table, __uint64_t* S_Table, __uint8_t* A_Table){
+  return mantissa_to_double(three_table_procedure(x,T_Table,S_Table,A_Table));
+}
+
+int calculate_id(unsigned __int128 x){
+  __uint64_t leadingBit = ((1ull << (fracBits+1)) & x) == 0 ? 0 : 1;
+  __uint64_t newLowu = leadingBit == 0 ? lowu : lowu + 1;
+  __uint64_t idx = ((1ull<<highf)-1) & (((__uint64_t) x) >> newLowu);
+  idx += leadingBit << highf;
+  return (int)idx;
+}
